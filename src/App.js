d8b1085e7ae7edc243b6c3332b4e2c85acf0fc8d
Geoffrey <geoffrpayton@gmail.com>
2021-05-17 14:33:25 -0400

    added ul element

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App() {
       <h1>Deploying with Netlify</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis veniam quod atque minima sed sunt, eveniet adipisci. Soluta pariatur eligendi totam nulla veniam! Vero, est!</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis harum exercitationem debitis ratione, culpa rerum provident eius libero nihil assumenda facilis quod ut suscipit cum corporis dolore minus quas consectetur voluptatum illum vero! Corporis, voluptatem sapiente! Fuga et repellendus culpa odit dignissimos, dolores architecto suscipit, dolore excepturi aliquam laborum mollitia!</p>
+
+      <h2>List Time!</h2>
+      <ul>
+        <li>This</li>
+        <li>is</li>
+        <li>a</li>
+        <li>list!</li>
+      </ul>
     </div>
   );
 }
